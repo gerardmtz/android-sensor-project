d19671d09321b954190bbd4db2ae279bc6662af7
Gerardo Martinez <gerartmartinez@gmail.com>
2024-11-12 16:21:42 -0600

    :sparkles: feat: commits first version of the app

--- AppSensorMagnetico/app/src/main/java/com/example/appsensormagnetico/MainActivity.kt
+++ AppSensorMagnetico/app/src/main/java/com/example/appsensormagnetico/MainActivity.kt
@@ -7,6 +7,8 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -20,8 +22,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     lateinit var imageView: ImageView
     val mGravity = FloatArray(3)
     val mGeomagnetic = FloatArray(3)
-    val azimuth: Float = 0f
-    val currentAzimuth: Float = 0f
+    var azimuth: Float = 0f
+    var currentAzimuth: Float = 0f
     lateinit var mSensorManager: SensorManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,7 +48,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     // the sensor events are delivered to your
     // application.
 
-    // Sets the sensor uptdate rate to
+    // Sets the sensor update rate to
     // 20 miliseconds per update
 
     override fun onResume() {
@@ -63,8 +65,6 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     override fun onPause() {
         super.onPause()
         mSensorManager.unregisterListener(this)
-
-
     }
 
     override fun onSensorChanged(sensorEvent: SensorEvent) {
@@ -72,14 +72,44 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
         if (sensorEvent.sensor.type == Sensor.TYPE_ACCELEROMETER) {
             mGravity[0] = alpha*mGravity[0]+(1-alpha)*sensorEvent.values[0];
-            
+            mGravity[1] = alpha*mGravity[1]+(1-alpha)*sensorEvent.values[1];
+            mGravity[2] = alpha*mGravity[2]+(1-alpha)*sensorEvent.values[2];
         }
 
+        if (sensorEvent.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            mGeomagnetic[0] = alpha*mGeomagnetic[0]+(1-alpha)*sensorEvent.values[0];
+            mGeomagnetic[1] = alpha*mGeomagnetic[1]+(1-alpha)*sensorEvent.values[1];
+            mGeomagnetic[2] = alpha*mGeomagnetic[2]+(1-alpha)*sensorEvent.values[2];
+        }
+
+        val R = FloatArray(9);
+        val I = FloatArray(9);
+
+        val success: Boolean = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+
+        if (success) {
+            val orientation = FloatArray(3);
+            SensorManager.getOrientation(R, orientation);
+            azimuth = Math.toDegrees(orientation[0].toDouble()).toFloat()
+            azimuth = (azimuth + 360) % 360;
+
+            // Animate the compass rotation
+            val anim: Animation = RotateAnimation(-currentAzimuth, -azimuth, Animation.RELATIVE_TO_SELF,
+                0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+
+            currentAzimuth = azimuth;
+
+            anim.duration = 500;
+            anim.repeatCount = 0;
+            anim.fillAfter= true;
+
+            imageView.startAnimation(anim);
+        }
 
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        TODO("Not yet implemented")
+
     }
 
 }
